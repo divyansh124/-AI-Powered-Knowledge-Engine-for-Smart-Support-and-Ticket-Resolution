d5959164afee924b2ad492ea36824391a9abb97f
divyansh124 <divyansh1200400@gmail.com>
2025-08-18 00:47:10 +0530

    Gitingore file created and python and md file also

---
+++ doc.md
@@ -0,0 +1,8 @@
+step 1
+--python -m venv [name of myenv]
+step 2
+run the enviornment
+myenv/Scripts/activate
+step 3
+pip install groq
+pip install langchain
---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+    temperature=0.5,
+)
+
+print(chat_completion.choices[0].message.content)
